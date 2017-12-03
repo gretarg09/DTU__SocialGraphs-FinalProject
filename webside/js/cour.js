9e7db9c07a36ed26f81dfc26a4fed2fc21a26602
IngvarBjarki <ingvarbjarki@gmail.com>
2017-12-03 20:28:09 +0100

    vefsida

---
+++ webside/js/cour.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  console.log( "ready!" );
+  console.log( "ready!" );
+  console.log( "ready!" );
+  console.log( "ready!" );
+  console.log( "ready!" );
+  console.log( "ready!" );
+});
+        $(document).ready(function(){
+          $('.carousel').carousel({
+            interval: 3000
+          })
+        });    
